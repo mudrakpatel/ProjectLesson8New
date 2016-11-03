2c97c66f4ff0de0078b39e705e5dedc31cd283b9
mudrakpatel <mudrakpatel96@gmail.com>
2016-11-02 20:01:22 -0500

    Add project files.

---
+++ ProjectLesson8New/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectLesson8New.MvcApplication" Language="C#" %>
---
+++ ProjectLesson8New/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectLesson8New.Startup))]
+namespace ProjectLesson8New
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
